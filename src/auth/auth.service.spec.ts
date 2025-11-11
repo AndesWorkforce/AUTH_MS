1002f0c910425d27297dff654493dcf8873c5660
David Morcillo <aljodavid100@gmail.com>
2025-11-11 16:25:47 -0500

    feat: implement custom exceptions system

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,33 @@
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
+  const mockJwtService = {
+    sign: jest.fn(),
+    verify: jest.fn(),
+  };
+
+  const mockUserService = {
+    send: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: JwtService,
+          useValue: mockJwtService,
+        },
+        {
+          provide: 'USER_SERVICE',
+          useValue: mockUserService,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
